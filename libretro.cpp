b5675710ea16f53b2405046cd1375f15183d220e
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-20 15:13:07 -0500

    Update libretro.cpp

--- libretro.cpp
+++ libretro.cpp
@@ -337,10 +337,48 @@ void retro_run(void)
    unsigned height = spec.DisplayRect.h;
 
 #if defined(WANT_32BPP)
-   const uint32_t *pix = surf->pixels;
+   uint32_t *pix = (uint32_t *)surf->pixels;
+
+   if (pix && width && height)
+   {
+      int row_pixels = (FB_WIDTH << 2) / sizeof(uint32_t);
+      for (unsigned y = 0; y < height; y++)
+      {
+         uint32_t *row = pix + (y * row_pixels);
+         for (unsigned x = 0; x < width; x++)
+         {
+            uint32_t p = row[x];
+            // Swap Red and Blue in 32-bit (ARGB / ABGR format adjustment)
+            uint32_t a = p & 0xFF000000;
+            uint32_t r = (p & 0x00FF0000) >> 16;
+            uint32_t g = (p & 0x0000FF00);
+            uint32_t b = (p & 0x000000FF) << 16;
+            row[x] = a | b | g | r;
+         }
+      }
+   }
+
    video_cb(pix, width, height, FB_WIDTH << 2);
 #elif defined(WANT_16BPP)
-   const uint16_t *pix = surf->pixels16;
+   uint16_t *pix = surf->pixels16;
+   
+   if (pix && width && height)
+   {
+      int row_pixels = (FB_WIDTH << 1) / sizeof(uint16_t);
+      for (unsigned y = 0; y < height; y++)
+      {
+         uint16_t *row = pix + (y * row_pixels);
+         for (unsigned x = 0; x < width; x++)
+         {
+            uint16_t p = row[x];
+            uint16_t r = (p & 0xF800) >> 11;
+            uint16_t g = ((p & 0x07E0) >> 1) & 0x03E0;
+            uint16_t b = (p & 0x001F) << 10;
+            row[x] = 0x8000 | b | g | r;
+         }
+      }
+   }
+
    video_cb(pix, width, height, FB_WIDTH << 1);
 #endif
 
