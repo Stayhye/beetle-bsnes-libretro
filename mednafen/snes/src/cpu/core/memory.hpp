1295b24f5437482a580be53ed0ec49f63ca4dd5d
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-20 14:31:00 -0500

    Update memory.hpp

--- mednafen/snes/src/cpu/core/memory.hpp
+++ mednafen/snes/src/cpu/core/memory.hpp
@@ -3,8 +3,9 @@ alwaysinline uint8_t op_readpc() {
 }
 
 alwaysinline uint8_t op_readstack() {
+  uint16_t addr = regs.s.w;
   regs.e ? regs.s.l++ : regs.s.w++;
-  return op_read(regs.s.w);
+  return op_read(addr);
 }
 
 alwaysinline uint8_t op_readstackn() {
@@ -12,7 +13,7 @@ alwaysinline uint8_t op_readstackn() {
 }
 
 alwaysinline uint8_t op_readaddr(uint32_t addr) {
-  return op_read(addr & 0xffff);
+  return op_read((uint16_t)addr);
 }
 
 alwaysinline uint8_t op_readlong(uint32_t addr) {
@@ -24,19 +25,20 @@ alwaysinline uint8_t op_readdbr(uint32_t addr) {
 }
 
 alwaysinline uint8_t op_readpbr(uint32_t addr) {
-  return op_read((regs.pc.b << 16) + (addr & 0xffff));
+  return op_read((regs.pc.b << 16) | (addr & 0xffff));
 }
 
 alwaysinline uint8_t op_readdp(uint32_t addr) {
-  if(regs.e && regs.d.l == 0x00) {
-    return op_read((regs.d & 0xff00) + ((regs.d + (addr & 0xffff)) & 0xff));
-  } else {
-    return op_read((regs.d + (addr & 0xffff)) & 0xffff);
+  uint32_t base = regs.d;
+  if(regs.e) {
+    // Optimized emulation mode direct page wrap (zero-page wrapping within bank 0)
+    return op_read((base & 0xff00) | ((base + (uint16_t)addr) & 0xff));
   }
+  return op_read((base + (uint16_t)addr) & 0xffff);
 }
 
 alwaysinline uint8_t op_readsp(uint32_t addr) {
-  return op_read((regs.s + (addr & 0xffff)) & 0xffff);
+  return op_read((regs.s + (uint16_t)addr) & 0xffff);
 }
 
 alwaysinline void op_writestack(uint8_t data) {
@@ -49,7 +51,7 @@ alwaysinline void op_writestackn(uint8_t data) {
 }
 
 alwaysinline void op_writeaddr(uint32_t addr, uint8_t data) {
-  op_write(addr & 0xffff, data);
+  op_write((uint16_t)addr, data);
 }
 
 alwaysinline void op_writelong(uint32_t addr, uint8_t data) {
@@ -61,17 +63,18 @@ alwaysinline void op_writedbr(uint32_t addr, uint8_t data) {
 }
 
 alwaysinline void op_writepbr(uint32_t addr, uint8_t data) {
-  op_write((regs.pc.b << 16) + (addr & 0xffff), data);
+  op_write((regs.pc.b << 16) | (addr & 0xffff), data);
 }
 
 alwaysinline void op_writedp(uint32_t addr, uint8_t data) {
-  if(regs.e && regs.d.l == 0x00) {
-    op_write((regs.d & 0xff00) + ((regs.d + (addr & 0xffff)) & 0xff), data);
+  uint32_t base = regs.d;
+  if(regs.e) {
+    op_write((base & 0xff00) | ((base + (uint16_t)addr) & 0xff), data);
   } else {
-    op_write((regs.d + (addr & 0xffff)) & 0xffff, data);
+    op_write((base + (uint16_t)addr) & 0xffff, data);
   }
 }
 
 alwaysinline void op_writesp(uint32_t addr, uint8_t data) {
-  op_write((regs.s + (addr & 0xffff)) & 0xffff, data);
+  op_write((regs.s + (uint16_t)addr) & 0xffff, data);
 }
